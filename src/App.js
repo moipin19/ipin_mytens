f65b0ef26b148163d9826ab775bdf586f9b50ae7
Mohamad Nur Arifin <moipin19@gmail.com>
2021-06-11 21:25:23 +0700

    #2 fetching data from random github user api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Form, Card, Image, Icon } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [followers, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [createdAt, setCreatedAt] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Abdullah')
+    .then(res => res.json())
+    .then(data =>{
+      // console.log(data);
+      setData(data);
+    });
+  }, []);
+
+  const setData = ({ name, login, followers, following, public_repos, avatar_url, created_at}) => {
+    setName(name);
+    setUserName(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+    setCreatedAt(created_at);
+  }
+
   return (
     <div>
       <div className="navbar">MyTens Assesment Project</div>
@@ -19,20 +46,33 @@ function App() {
       </div>
       <div className="card">
         <Card>
-          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Image src= {avatar} wrapped ui={false} />
           <Card.Content>
-            <Card.Header>Matthew</Card.Header>
+            <Card.Header>{name}</Card.Header>
+            {/* <Card.Header>{userName}</Card.Header> */}
             <Card.Meta>
-              <span className='date'>Joined in 2015</span>
+              <span className='date'>Joined in {createdAt}</span>
             </Card.Meta>
-            <Card.Description>
+            {/* <Card.Description>
               Matthew is a musician living in Nashville.
-            </Card.Description>
+            </Card.Description> */}
           </Card.Content>
           <Card.Content extra>
             <a>
-              <Icon name='user' />
-              22 Friends
+              <Icon name='users' />
+              {followers} followers
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user plus' />
+              {following} following
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='archive' />
+              {repos} repos
             </a>
           </Card.Content>
         </Card>
