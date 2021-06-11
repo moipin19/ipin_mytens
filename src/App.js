3e6713b96cf25803e01d407e00c2d945ad04ecc2
Mohamad Nur Arifin <moipin19@gmail.com>
2021-06-11 20:04:45 +0700

    #1 Try semantic ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form, Card, Image, Icon } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">MyTens Assesment Project</div>
+      <div className="search">
+        <Form>
+          <Form.Group>
+            <Form.Input
+              placeholder='Name'
+              name='name'
+            />
+            <Form.Button content='Submit' />
+          </Form.Group>
+        </Form>
+      </div>
+      <div className="card">
+        <Card>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+      </div>
     </div>
   );
 }
