ea98d431ce260a397102cc6199dc05262e019789
Mohamad Nur Arifin <moipin19@gmail.com>
2021-06-12 07:46:32 +0700

    #3 create component to show repositories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from 'react';
 import { Form, Card, Image, Icon } from 'semantic-ui-react'
 import './App.css';
+import Repos from './Repos';
 
 function App() {
   const [name, setName] = useState('');
@@ -10,17 +11,19 @@ function App() {
   const [repos, setRepos] = useState('');
   const [avatar, setAvatar] = useState('');
   const [createdAt, setCreatedAt] = useState('')
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('https://api.github.com/users/Abdullah')
-    .then(res => res.json())
-    .then(data =>{
-      // console.log(data);
-      setData(data);
-    });
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        setData(data);
+      });
   }, []);
 
-  const setData = ({ name, login, followers, following, public_repos, avatar_url, created_at}) => {
+  const setData = ({ name, login, followers, following, public_repos, avatar_url, created_at }) => {
     setName(name);
     setUserName(login);
     setFollowers(followers);
@@ -30,53 +33,75 @@ function App() {
     setCreatedAt(created_at);
   }
 
+  const handleSearch = (event) => {
+    setUserInput(event.target.value);
+    console.log(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.message) {
+          setError(data.message)
+        } else {
+          setError(null);
+          setData(data);
+        }
+      })
+  }
+
   return (
     <div>
       <div className="navbar">MyTens Assesment Project</div>
       <div className="search">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Input
               placeholder='Name'
-              name='name'
+              name='name' onChange={handleSearch}
             />
             <Form.Button content='Submit' />
           </Form.Group>
         </Form>
       </div>
-      <div className="card">
-        <Card>
-          <Image src= {avatar} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{name}</Card.Header>
-            {/* <Card.Header>{userName}</Card.Header> */}
-            <Card.Meta>
-              <span className='date'>Joined in {createdAt}</span>
-            </Card.Meta>
-            {/* <Card.Description>
-              Matthew is a musician living in Nashville.
-            </Card.Description> */}
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name='users' />
-              {followers} followers
-            </a>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name='user plus' />
-              {following} following
-            </a>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name='archive' />
-              {repos} repos
-            </a>
-          </Card.Content>
-        </Card>
-      </div>
+      {error ? (<h1 className="error"> User "{userInput}" {error} </h1>) : (
+        <div className="card">
+          <Card>
+            <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{name}</Card.Header>
+              {/* <Card.Header>{userName}</Card.Header> */}
+              <Card.Meta>
+                <span className='date'>Joined in {createdAt}</span>
+              </Card.Meta>
+              {/* <Card.Description>
+                    Matthew is a musician living in Nashville.
+                  </Card.Description> */}
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='users' />
+                {followers} followers
+                  </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user plus' />
+                {following} following
+                  </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='archive' />
+                {repos} repos
+                  </a>
+            </Card.Content>
+          </Card>
+        </div>
+      )}
+      
+      <Repos />
     </div>
   );
 }
---
+++ src/Repos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Repos = () => {
+    return(
+        <div>
+            Halo cuk
+        </div>
+    )
+}
+
+export default Repos
